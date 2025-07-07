10995d73c6ef4bd77faf7e4fef223a1cddc24304
Adonis <adonai-lucai@hotmail.com>
2025-07-07 14:57:52 -0300

    Service layer, component registration

--- src/main/java/com/dev/spring/repositories/UserRepository.java
+++ src/main/java/com/dev/spring/repositories/UserRepository.java
@@ -2,7 +2,9 @@ package com.dev.spring.repositories;
 
 import com.dev.spring.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 
